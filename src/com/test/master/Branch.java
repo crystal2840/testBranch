fe3eb674a4979c5575c1859e66675dc394f2f363
crystal2840 <crystal2840@mobigen.com>
2017-09-22 13:30:27 +0900

    지움

--- src/com/test/master/Branch.java
+++ src/com/test/master/Branch.java
@@ -5,8 +5,7 @@ public class Branch {
 	public static void main(String[] args) {
 		System.out.println("test!!!!!!!branch");
 		System.out.println("test!!!!!!!branch1");
-		System.out.println("test!!!!!!!branch2");
-		System.out.println("test!!!!!!!branch3");
+
 	}
 
 }
