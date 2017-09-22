bc01a28546b0bc53cf5bd2310b86cd67c75940a5
crystal2840 <crystal2840@mobigen.com>
2017-09-22 13:22:29 +0900

    test

--- src/com/test/master/Master.java
+++ src/com/test/master/Master.java
@@ -5,6 +5,7 @@ public class Master {
 	public static void main(String[] args) {
 		System.out.println("master!!");
 		System.out.println("branch!!!test");
+		System.out.println("branch!!! master!!!! test2");
 	}
 
 }
