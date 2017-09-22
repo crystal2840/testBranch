af3270cbf1d1812c54ae15575977713dc00d863a
crystal2840 <crystal2840@mobigen.com>
2017-09-22 13:14:31 +0900

    branch2,3 입력

--- src/com/test/master/Branch.java
+++ src/com/test/master/Branch.java
@@ -5,6 +5,8 @@ public class Branch {
 	public static void main(String[] args) {
 		System.out.println("test!!!!!!!branch");
 		System.out.println("test!!!!!!!branch1");
+		System.out.println("test!!!!!!!branch2");
+		System.out.println("test!!!!!!!branch3");
 	}
 
 }
