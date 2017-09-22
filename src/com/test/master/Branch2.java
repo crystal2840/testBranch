295c3efd9d0c9441e1294fe4a1984391cbf69b63
crystal2840 <crystal2840@mobigen.com>
2017-09-22 13:45:28 +0900

    test 11111111

--- src/com/test/master/Branch2.java
+++ src/com/test/master/Branch2.java
@@ -3,6 +3,12 @@ package com.test.master;
 public class Branch2 {
 	public static void main(String[] args) {
 		System.out.println("branch 2!!!!!!!!!!!!!");
+		
+		
+		//test
+		System.out.println("branch 2!!!!!!!!!!!!!");
+		System.out.println("branch 2!!!!!!!!!!!!!");
+		System.out.println("branch 2!!!!!!!!!!!!!");
 	}
 
 }
